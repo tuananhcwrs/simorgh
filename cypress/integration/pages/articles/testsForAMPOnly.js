5c014f21e7f4226539d92e2c36feedebc017b8d9
Karina Thomas <karina.thomas@bbc.co.uk>
2020-04-30 23:39:24 +0100

    Reinstate deleted media playback test

--- cypress/integration/pages/articles/testsForAMPOnly.js
+++ cypress/integration/pages/articles/testsForAMPOnly.js
@@ -1,5 +1,7 @@
+import appConfig from '../../../../src/server/utilities/serviceConfigs';
 import envConfig from '../../../support/config/envs';
 import appToggles from '../../../support/helpers/useAppToggles';
+import { getBlockData, getVideoEmbedUrl } from './helpers';
 
 // For testing important features that differ between services, e.g. Timestamps.
 // We recommend using inline conditional logic to limit tests to services which differ.
@@ -9,6 +11,7 @@ export const testsThatAlwaysRunForAMPOnly = () => {};
 export const testsThatFollowSmokeTestConfigForAMPOnly = ({
   service,
   pageType,
+  variant,
 }) =>
   describe(`Running testsForAMPOnly for ${service} ${pageType}`, () => {
     if (appToggles.chartbeatAnalytics.enabled) {
@@ -18,6 +21,23 @@ export const testsThatFollowSmokeTestConfigForAMPOnly = ({
             cy.hasAmpChartbeatConfigUid();
           });
         }
+      });
+    }
+    // `appToggles` tells us whether a feature is toggled on or off in the current environment.
+    if (appToggles.mediaPlayer.enabled) {
+      describe('Media Player: AMP', () => {
+        it('should render an iframe with a valid URL', () => {
+          cy.request(`${Cypress.env('currentPath')}.json`).then(({ body }) => {
+            const media = getBlockData('video', body);
+
+            if (media && media.type === 'video') {
+              const { lang } = appConfig[service][variant];
+              const embedUrl = `${getVideoEmbedUrl(body, lang)}/amp`;
+              cy.get(`amp-iframe[src="${embedUrl}"]`).should('be.visible');
+              cy.testResponseCodeAndType(embedUrl, 200, 'text/html');
+            }
+          });
+        });
       });
     }
   });
