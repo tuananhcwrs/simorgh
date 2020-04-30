53b438224060ffec5eb479ca481c9448c900025a
Karina Thomas <karina.thomas@bbc.co.uk>
2020-04-30 22:27:29 +0100

    Adding canonical tests for the media player

--- src/integration/pages/articles/canonicalTests.js
+++ src/integration/pages/articles/canonicalTests.js
@@ -6,4 +6,17 @@ import {
 export default () => {
   runCoreCanonicalTests();
   runCanonicalAnalyticsTests();
+
+  describe('Media Player', () => {
+    const mediaPlaceholder = document.querySelector(
+      'main figure div[class^="StyledVideoContainer"]',
+    );
+
+    it('I can see a play button with guidance, video caption and duration', () => {
+      const playButtonEl = mediaPlaceholder.querySelector('button span');
+      expect(playButtonEl).toBeInTheDocument();
+      expect(playButtonEl.textContent).toBeTruthy();
+      expect(playButtonEl.textContent).toMatchSnapshot();
+    });
+  });
 };
