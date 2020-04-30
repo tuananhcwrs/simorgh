fb90c12f845b913d3d7c17b6563f54e9de9c6fc8
Karina Thomas <karina.thomas@bbc.co.uk>
2020-04-30 22:35:26 +0100

    Media placeholder image tests for AMP

--- src/integration/pages/articles/ampTests.js
+++ src/integration/pages/articles/ampTests.js
@@ -3,4 +3,16 @@ import { runCoreAmpTests, runMediaPlayerEmbedTests } from '../../common';
 export default () => {
   runCoreAmpTests();
   runMediaPlayerEmbedTests();
+
+  describe('Media player', () => {
+    it('should render a placeholder image', () => {
+      const mediaPlaceholderImage = document.querySelector(
+        'amp-iframe amp-img',
+      );
+
+      expect(mediaPlaceholderImage).toBeInTheDocument();
+      expect(mediaPlaceholderImage.getAttribute('src')).toBeTruthy();
+      expect(mediaPlaceholderImage.getAttribute('src')).toMatchSnapshot();
+    });
+  });
 };
