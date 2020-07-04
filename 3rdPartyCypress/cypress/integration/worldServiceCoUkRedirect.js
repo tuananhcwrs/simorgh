12702c6e4eea7efa0bbdbd43c569fe17ea212137
Karina Thomas <karina.thomas@bbc.co.uk>
2020-07-04 13:32:45 +0100

    Implement tests for test.bbc.co.uk redirects

--- 3rdPartyCypress/cypress/integration/worldServiceCoUkRedirect.js
+++ 3rdPartyCypress/cypress/integration/worldServiceCoUkRedirect.js
@@ -1,3 +1,4 @@
+import Url from 'url-parse';
 import services from '../../../src/server/utilities/serviceConfigs';
 
 describe('WS Redirects', () => {
@@ -22,28 +23,27 @@ describe('WS Redirects', () => {
       return;
     }
 
-    it(`should redirect *bbc.com/${service}`, () => {
+    const baseDomain = Cypress.env('APP_ENV') === 'test' ? 'test.' : '';
+
+    it(`www.${baseDomain}bbc.co.uk/${service} should redirect to www.${baseDomain}bbc.com/${service}`, () => {
       const urlsTotest = [
-        `https://www.bbc.co.uk/${service}`,
-        `https://www.bbc.co.uk/${service}/articles/a0000000000o`,
-        `https://www.bbc.co.uk/${service}/articles/a0000000000o.amp`,
+        `https://www.${baseDomain}bbc.co.uk/${service}`,
+        `https://www.${baseDomain}bbc.co.uk/${service}/articles/a0000000000o`,
+        `https://www.${baseDomain}bbc.co.uk/${service}/articles/a0000000000o.amp`,
       ];
 
       urlsTotest.forEach(urlToTest => {
-        const slashLoc = urlToTest.indexOf('/', 8);
         cy.request({
           url: urlToTest,
           followRedirect: false,
         }).then(resp => {
           expect(resp.status).to.eq(301);
-          // expect first slice to equal https://www.bbc.com/
-          expect(resp.redirectedToUrl.substring(0, 20)).to.eq(
-            'https://www.bbc.com/',
-          );
-          // expect second slice to equal whatever came in
-          expect(resp.redirectedToUrl.substring(20)).to.eq(
-            urlToTest.substring(slashLoc + 1),
-          );
+          const redirected = new Url(resp.redirectedToUrl);
+          expect(redirected.origin).to.eq(`https://www.${baseDomain}bbc.com`);
+
+          const original = new Url(urlToTest);
+          // expect redirected path to the same as the url under test
+          expect(original.pathname).to.eq(redirected.pathname);
         });
       });
     });
